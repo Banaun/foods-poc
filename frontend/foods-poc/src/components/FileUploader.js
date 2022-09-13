8de9ff13fc87270da52607c5b1f6b42f1462bf83
Banaun <emil.mogensen87@gmail.com>
2022-09-13 22:37:11 +0200

    added functionality for showing svg from array of characters

--- frontend/foods-poc/src/components/FileUploader.js
+++ frontend/foods-poc/src/components/FileUploader.js
@@ -1,54 +1,56 @@
 import UploadService from "../services/upload-files-service";
 
-const FileUploader = ({selectedFile, setSelectedFile, retrievable, setRetrievable}) => {
-    function handleChange(e) {
-        setSelectedFile(e.target.files[0]);
-        setRetrievable(false);
+const FileUploader = ({
+  selectedFile,
+  setSelectedFile,
+  retrievable,
+  setRetrievable,
+}) => {
+  function handleChange(e) {
+    setSelectedFile(e.target.files[0]);
+    setRetrievable(false);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    //Check if user has chosen a file
+    if (selectedFile) {
+      //Upload the selected file to backend
+      UploadService.upload(selectedFile)
+        .then((res) => {
+          //Update state for retrievable text
+          console.log(res);
+          if (res.data.message !== "Could not upload the file.") {
+            setRetrievable(true);
+            alert(res.data.message);
+          } else {
+            setRetrievable(false);
+            alert(res.data.message);
+          }
+        })
+        .catch((res) => {
+          console.log(res);
+        });
+    } else {
+      alert("No file chosen!");
     }
-    
-    function handleSubmit(e) {
-        e.preventDefault();
-        //Check if user has chosen a file
-        if (selectedFile) {
-          //Upload the selected file to backend
-          UploadService.upload(selectedFile)
-            .then((res) => {
-              //Update state for retrievable text
-              if (res.data.message == "Could not upload the file.") {
-                alert(res.data.message);
-              } else {
-                setRetrievable(true);
-                alert(res.data.message);
-              }
-            })
-            .catch((res) => {
-              console.log(res);
-            });
-        } else {
-          alert("No file chosen!");
-        }
-        //Clear the input field (just looks better)
-        e.target.reset();
-    }
-    
-    return (
-        <div className="upload-container">
-            <form className="upload-form" onSubmit={handleSubmit}>
-                <input
-                    type="file"
-                    accept=".rtf, .md, .txt, .file, .xlsx"
-                    onChange={handleChange}
-                />
-                <button type="submit">Upload</button>
-            </form>
-            {retrievable 
-                ? 
-                <p>Uploaded file: {selectedFile.name}</p> 
-                : 
-                <></>
-            }
-        </div>
-    );
-}
+    //Clear the input field (just looks better)
+    e.target.reset();
+  }
+
+  return (
+    <div className="upload-container">
+      <form className="upload-form" onSubmit={handleSubmit}>
+        <input
+          type="file"
+          accept=".rtf, .md, .txt, .file, .xlsx"
+          onChange={handleChange}
+        />
+        <button type="submit">Upload</button>
+      </form>
+      {retrievable ? <p>Uploaded file: {selectedFile.name}</p> : <></>}
+    </div>
+  );
+};
 
 export default FileUploader;
--- frontend/foods-poc/src/components/OutputField.js
+++ frontend/foods-poc/src/components/OutputField.js
@@ -1,50 +1,79 @@
 import React, { useState } from "react";
 import ProcessFileService from "../services/process-file-service";
+import { ReactComponent as SvgSandwich } from "../assets/images/IKEAfoods-01.svg";
+import { ReactComponent as SvgBookcase } from "../assets/images/IKEAfoods-02.svg";
 
-const OutputField = ({selectedFile, retrievable, setRetrievable}) => {
-    const [text, setText] = useState();
-    const [loading, setLoading] = useState(false);
+const OutputField = ({ selectedFile, retrievable, setRetrievable }) => {
+  const [textArr, setTextArr] = useState([]);
 
-    function processText(e) {
-        e.preventDefault();
-        //Check if user has uploaded a file
-        if (retrievable) {
-            //Set loading to true (for big files)
-            setLoading(true);
-            //Get the processed text from backend
-            ProcessFileService.process(selectedFile.name).then((res) => {
-            setText(res.data.message);
-            setRetrievable(false);
-            setLoading(false);
-            });
-        } else {
-            alert("You need to upload a file first.");
-        }
+  function processText(e) {
+    e.preventDefault();
+    //Check if user has uploaded a file
+    if (retrievable) {
+      //Get the processed text from backend
+      ProcessFileService.process(selectedFile.name).then((res) => {
+        replaceValuesWithSvg(res.data.message);
+        setRetrievable(false);
+      });
+    } else {
+      alert("You need to upload a file first.");
     }
+  }
 
-    function downloadImage() {
-        console.log("Downloading...")
+  function replaceValuesWithSvg(retrievedText) {
+    //Separate characters into an array
+    let textArr = [...retrievedText];
+    let tempTextArr = [];
+
+    for (let i = 0; i < textArr.length; i++) {
+      if (textArr[i] === "x") {
+        tempTextArr.push("x");
+      } else if (textArr[i] === "v") {
+        tempTextArr.push("v");
+      }
     }
+    setTextArr([...tempTextArr]);
+  }
 
-    return (
-        <div className="output-outer-container">
-            <div className="upload-form-button-group">
-                <button className="retrieve-text-button" onClick={processText}>
-                    Retrieve file
-                </button>
-                <button className="download-image-button" onClick={downloadImage}>
-                    Download image
-                </button>
-            </div>
-            <div className="output-container">
-                {loading ? (
-                    <div className="loader"></div>
-                ) : (
-                    <p className="output">{text}</p>
-                )}
-            </div>
+  function downloadImage() {}
+
+  /*
+  const items = textArr.map((item) => (
+    <>
+      {item === "x" ? (
+        <SvgSandwich width="2rem" />
+      ) : (
+        <SvgBookcase width="2rem" />
+      )}
+    </>
+  ));
+  */
+
+  return (
+    <div className="output-outer-container">
+      <div className="upload-form-button-group">
+        <button className="retrieve-text-button" onClick={processText}>
+          Retrieve file
+        </button>
+        <button className="download-image-button" onClick={downloadImage}>
+          Download image
+        </button>
+      </div>
+      <div className="output-container">
+        <div className="output">
+          {textArr.map((item) => {
+            if (item === "x") {
+              return <SvgSandwich width="3rem" />;
+            } else if (item === "v") {
+              return <SvgBookcase width="3rem" />;
+            } else {
+              <></>;
+            }
+          })}
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default OutputField;
